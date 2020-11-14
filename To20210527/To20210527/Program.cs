1b3e84e3e8b31fb6c123dfa066c21d0f5d54cb30
pm1975 <pm1975@windowslive.com>
2020-11-14 23:56:01 +0100

    ToDoList update

--- To20210527/To20210527/Program.cs
+++ To20210527/To20210527/Program.cs
@@ -9,7 +9,7 @@ namespace To20210527
         {
             string v = "";
             string v1 = "";
-            int tasks = 593;
+            int tasks = 598;
             try
             {
                 v = args[0];
@@ -48,27 +48,38 @@ namespace To20210527
             Console.WriteLine("{0} - {1} = {2}", 3 * odds.Days, 1200 - tasks, (3 * odds.Days) - (1200 - tasks));
             Console.WriteLine((1200.0 - tasks)/odds.Days);
             Console.WriteLine("");
-            Console.WriteLine("{0} + ({1}/7 * (0 * 5 + 6 * 2)) - 1200 = {2}"
-                , tasks
-                , odds.Days
-                , tasks + (odds.Days/7 * (0 * 5 + 6 * 2)) - 1200);
-            Console.WriteLine("{0} + ({1}/7 * (1 * 5 + 5 * 2)) - 1200 = {2}"
-                , tasks
-                , odds.Days
-                , tasks + (odds.Days/7 * (1 * 5 + 5 * 2)) - 1200);
-            Console.WriteLine("{0} + ({1}/7 * (2 * 5 + 4 * 2)) - 1200 = {2}"
-                , tasks
-                , odds.Days
-                , tasks + (odds.Days/7 * (2 * 5 + 4 * 2)) - 1200);
-            Console.WriteLine("{0} + ({1}/7 * (3 * 5 + 3 * 2)) - 1200 = {2}"
-                , tasks
-                , odds.Days
-                , tasks + (odds.Days/7 * (3 * 5 + 3 * 2)) - 1200);
-            Console.WriteLine("{0} + ({1}/7 * (4 * 5 + 1 * 2)) - 1200 = {2}"
-                , tasks
-                , odds.Days
-                , tasks + (odds.Days/7 * (4 * 5 + 1 * 2)) - 1200);
+            PrintOptions(tasks, odds.Days);
             Console.ReadKey();
+        }
+
+        static void PrintOptions(int tasks, int days)
+        {
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 0 * 5 + 5 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 0 * 5 + 5 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 1 * 5 + 3 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 1 * 5 + 3 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 0 * 5 + 6 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 0 * 5 + 6 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (1 * 9 + 0 * 5 + 2 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (1 * 9 + 0 * 5 + 2 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (1 * 9 + 1 * 5 + 0 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (1 * 9 + 1 * 5 + 0 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 1 * 5 + 5 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 1 * 5 + 5 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 2 * 5 + 3 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 2 * 5 + 3 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 3 * 5 + 1 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 3 * 5 + 1 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 2 * 5 + 4 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (0 * 9 + 2 * 5 + 4 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (1 * 9 + 2 * 5 + 0 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (1 * 9 + 2 * 5 + 0 * 2)) - 1200);
+            Console.WriteLine("{0} + ({1}/7 * (2 * 9 + 0 * 5 + 1 * 2)) - 1200 = {2}",
+             tasks, days, tasks + (days / 7 * (2 * 9 + 0 * 5 + 1 * 2)) - 1200);
+            int result = tasks + (days / 7 * (0 * 9 + 3 * 5 + 3 * 2)) - 1200;
+            if (result < 0) Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 3 * 5 + 3 * 2)) - 1200 = {2}", tasks, days, result);
+            result = tasks + (days / 7 * (0 * 9 + 4 * 5 + 1 * 2)) - 1200;
+            if (result < 0) Console.WriteLine("{0} + ({1}/7 * (0 * 9 + 4 * 5 + 1 * 2)) - 1200 = {2}", tasks, days, result);
         }
     }
 }
