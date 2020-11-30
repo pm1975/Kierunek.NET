5067b5a15e8e5847fa0c14cd3eb5eaa6b5e7f6b1
Piotrek <piotrek@iMac-MC212.local>
2020-11-30 08:34:50 +0100

    git init pierwsza_mobilna (on mac)

---
+++ pierwsza_mobilna/ToDoList/ToDoList/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
