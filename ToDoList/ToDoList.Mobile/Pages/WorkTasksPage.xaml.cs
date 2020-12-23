bded2084739733ebec7f5a7de33833ebcd887e43
macosami <pm1975@windowslive.com>
2020-12-23 14:30:56 +0100

    ToDoList.Mobile. Finished :) (on mac)

---
+++ ToDoList/ToDoList.Mobile/Pages/WorkTasksPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ToDoList.Core;
+using Xamarin.Forms;
+
+namespace ToDoList.Mobile
+{
+    public partial class WorkTasksPage : ContentPage
+    {
+        public WorkTasksPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new WorkTasksPageViewModel();
+        }
+    }
+}
